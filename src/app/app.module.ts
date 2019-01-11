35ae67469b292b360f4f9ef6816053e2b872a213
abraham <abrtek2015@gmail.com>
2019-01-10 21:13:53 -0800

    week 1 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import { MatToolbarModule} from "@angular/material";
+import { MatToolbarModule} from "@angular/material/toolbar";
 import { FlexLayoutModule} from "@angular/flex-layout";
-import { MatListModule} from "@angular/material";
+import { MatListModule} from "@angular/material/list";
 import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { MatGridListModule} from "@angular/material/grid-list";
 import { MatCardModule} from "@angular/material/card";
 import { MatButtonModule} from "@angular/material/button";
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
     FlexLayoutModule,
+    MatToolbarModule,
     MatListModule,
     MatGridListModule,
     MatCardModule,
