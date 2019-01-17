0c8462b57c4b5404b0c4493c6ebaaf606813ed7f
abraham <abrtek2015@gmail.com>
2019-01-17 13:22:48 -0800

    reactive forms exercise 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,10 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule} from "@angular/material/input";
 import { MatCheckboxModule } from "@angular/material/checkbox";
 import { FormsModule} from "@angular/forms";
+import { ReactiveFormsModule} from "@angular/forms";
+
+import { MatSelectModule} from "@angular/material/select";
+import { MatSlideToggleModule} from "@angular/material/slide-toggle";
 
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
@@ -56,9 +60,12 @@ import { LoginComponent } from './login/login.component';
     MatButtonModule,
     MatDialogModule,
     FormsModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatSelectModule,
+    MatSlideToggleModule
   ],
   providers: [
     DishService,
